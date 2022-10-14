e77968ed004a34b1aaaa69cce0a619df475bc52c
Marcus Campbell <marcus.l.campbell@gmail.com>
2022-10-14 18:01:21 +0100

    add buttons and theme styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/index.html}"],
-    theme: {
-      extend: {},
+  content: ["src/index.html"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
     },
-    plugins: [],
-  }
-
-  
+  },
+  plugins: [],
+};
